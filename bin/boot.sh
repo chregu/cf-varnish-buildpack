9358a92a78fa4c64b134f7609b4b466e2e73c682
Christian Stocker <me@chregu.tv>
2015-01-22 14:41:48 +0100

    remove debug stuff

--- bin/boot.sh
+++ bin/boot.sh
@@ -20,8 +20,6 @@ if [ -z "$VARNISH_MEMORY_LIMIT" ]; then
     VARNISH_MEMORY_LIMIT=$MEMORY_LIMIT
 fi
 
-echo $VARNISH_MEMORY_LIMIT > /home/vcap/tmp/memorylimit.txt
-
 # TODO, Make MEMORY_LIMIT adjustable, this now comes from CF itself
 exec $APP_ROOT/varnish/sbin/varnishd -n /home/vcap/tmp/varnish -F -f $APP_ROOT/varnish/etc/varnish/default.vcl -a 0.0.0.0:$VCAP_APP_PORT -t 120 -w 50,1000,120 -s malloc,$VARNISH_MEMORY_LIMIT -T 127.0.0.1:6082 -p http_resp_hdr_len=32768 2>&1
 # ------------------------------------------------------------------------------------------------
