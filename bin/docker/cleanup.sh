a6235fd26590b3ee0eb2addfc6e8f4ea022dce71
Christian Stocker <me@chregu.tv>
2015-01-26 21:06:25 +0100

    don't include *.a and *.la files in the tgz

--- bin/docker/cleanup.sh
+++ bin/docker/cleanup.sh
@@ -9,6 +9,8 @@ rm -f $build/bin/pcre*
 rm -rf $build/share
 rm -rf $build/include
 rm -rf $build/lib/pkgconfig
+find $build/lib/ -name "*.a" -exec rm {} \;
+find $build/lib/ -name "*.la" -exec rm {} \;
 
 mkdir -p $build/share/doc/varnish
 cp $src/varnish-$varnish_version/{LICENSE,README} $build/share/doc/varnish
