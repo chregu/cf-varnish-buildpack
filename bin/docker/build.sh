c6aa7711c4a357ec99f996b071d4fdd317069c39
Christian Stocker <me@chregu.tv>
2015-07-07 14:57:24 +0200

    build varnish for cflinuxfs2 stack

--- bin/docker/build.sh
+++ bin/docker/build.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 ./install_packages.sh
-./compile_pcre.sh
+#./compile_pcre.sh
 ./compile_varnish.sh
 ./compile_vmod_statsd.sh
 ./compile_vmod_timers.sh
--- bin/docker/install_packages.sh
+++ bin/docker/install_packages.sh
@@ -1,5 +1,5 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install -fy build-essential zlib1g-dev wget git-core automake libtool pkg-config rst2man
+sudo apt-get install -fy build-essential zlib1g-dev wget git-core automake libtool pkg-config libpcre3-dev python-docutils
 
